cbf8b79e50c7815304e3377771cbb664cf76d38e
faroukfaiz10 <48885293+faroukfaiz10@users.noreply.github.com>
2019-04-19 23:19:25 +0200

    Adding comments

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 const { Client } = require('pg')
+
+// Informations for connecting to database
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
@@ -12,24 +14,34 @@ const client = new Client({
 
 client.connect()
 
+// For parsing request objects from post requests
 app.use(bodyParser.urlencoded({extended: true}));
+
+// For using EJS to generate html documents
 app.set("view engine", "ejs");
 
 packagesList=[];
 
+// Index page
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/packages", function(req, res){
+    // Retrieve data (packages) from database 
     client.query('SELECT * FROM colis JOIN students ON students.email_address = colis.email ORDER BY colis_id DESC', function(err, allPackages) {
+        // Render the 'packages' page after retrieving data
         res.render("packages", {packagesList:allPackages.rows});
     })
 })
 
+// Post request after adding new package
 app.post("/packages", function(req, res){
+    // Data received from the form
     args = [req.body.email, "En attente", req.body.sender]
+    // Adding data to packages table
     client.query('INSERT INTO colis (email, status, sender) VALUES ($1, $2, $3)', args, function (err,res2){
+        // Redirecting to packages page to display the package just added
         res.redirect("/packages");
     });
 })
