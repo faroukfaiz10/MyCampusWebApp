55e3a72ff92a302044cfff415df14a9bdebb11af
faroukfaiz10 <48885293+faroukfaiz10@users.noreply.github.com>
2019-04-27 07:47:32 +0200

    Clean main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,47 @@
+// Set the inputs values in the modify modal to the actual values of the package to modify
 $('#modifyPackage').on('show.bs.modal', function (event) {
     email = $(event.relatedTarget).data("email");
     sender = $(event.relatedTarget).data("sender");
     colis_id = $(event.relatedTarget).data("id");
     $(this).find(".form-group input[type=text]").val(sender);
     $(this).find(".form-group input[type=email]").val(email);
+    // Set the post request path
     $(this).find("form").attr("action", "/packages/" + colis_id);
 });
 
+// Set the post request path
 $('#deletePackage').on('show.bs.modal', function (event) {
     colis_id = $(event.relatedTarget).data("id");
     $(this).find("form").attr("action", "/packages/delete/" + colis_id);
 });
 
-
-
-var rowsPerPage = 5
+var rowsPerPage   = 5  // Starting value
 var numberOfRows  = $('ul.pagination').data("number_of_rows")
 var numberOfPages = Math.ceil(numberOfRows/rowsPerPage)
-
-
-
-
-var pagesHtml = "<li class='page-item'><a class='page-link bg-secondary text-light'>Précédent</a></li>"
-
-numberOfPages = Math.ceil(numberOfRows/rowsPerPage)
-for(var i=1; i <= numberOfPages; i++){
-    pagesHtml += "<li class=''><a class='page-link bg-secondary text-light page-number'>"+ i +"</a></li>"
-};
-pagesHtml += "<li class='page-item'><a class='page-link bg-secondary text-light'>Suivant</a></li>"
-$("ul.pagination").html(pagesHtml);
-
 var $tableRows    = $('table tbody tr');
+var pagesHtml     = ""
+
+// Generating Html for the pages links
+function pagesLinksHtml (){
+    pagesHtml = "<li class='page-item'><a class='page-link bg-secondary text-light'>Précédent</a></li>"
+    for(var i=1; i <= numberOfPages; i++){
+        pagesHtml += "<li class=''><a class='page-link bg-secondary text-light page-number'>"+ i +"</a></li>"
+    };
+    pagesHtml += "<li class='page-item'><a class='page-link bg-secondary text-light'>Suivant</a></li>"
+    $("ul.pagination").html(pagesHtml);
+}
+
+pagesLinksHtml()
+// Then create the variables related to pages links
 var $allLinks     = $('ul.pagination li a') // $allLinks = $pagesLinks + $nextLink + $previousLink
 var $pagesLinks   = $('ul.pagination li a.page-number')
 var $nextLink     = $($allLinks[$allLinks.length-1])
 var $previousLink = $($allLinks[0])
 var actualPage    = 1
 
-
-function eventlistenerlinks(){
+function eventsHandlingLinks(){
     // Event listener on the pages links
     $pagesLinks.on("click", function(event) {
-        console.log("hi")
         actualPage = $(this).text()
         // Start by hiding all rows
         $('table').find('tbody tr').hide();
@@ -86,7 +86,6 @@ function eventlistenerlinks(){
 
     $nextLink.on("click", function(){
         // Trigger the next page link
-        console.log("next")
         $($pagesLinks[actualPage]).trigger("click")
     })
 
@@ -95,10 +94,6 @@ function eventlistenerlinks(){
         $($pagesLinks[actualPage - 2]).trigger("click")
     })
 }
-
-
-
-
 
 // Start by hiding all rows
 $('table').find('tbody tr').hide();
@@ -113,36 +108,32 @@ $($pagesLinks[0]).addClass("bg-dark");
 $previousLink.parent().css("cursor", "default")
 $previousLink.parent().addClass("disabled")
 
-eventlistenerlinks()
+// Set up event handling for pages links
+eventsHandlingLinks()
 
+// Event handling when changing rowsPerPage parameter
 $('.custom-select').on("change", function(){
-    console.log("change")
-    actualPage    = 1
-    pagesHtml = "<li class='page-item'><a class='page-link bg-secondary text-light'>Précédent</a></li>"
-    rowsPerPage = parseInt($(this).val(),10)
+    rowsPerPage   = parseInt($(this).val(),10)
+    actualPage    = 1  //Return to page 1 when changing rowsPerPage
+    numberOfPages = Math.ceil(numberOfRows/rowsPerPage)
+
+    // Display first page rows
     $('table').find('tbody tr').hide();
     for (var i = 0; i <= rowsPerPage - 1; i++) {
         $($tableRows[i]).show();
     }
-    numberOfPages = Math.ceil(numberOfRows/rowsPerPage)
-    for(var i=1; i <= numberOfPages; i++){
-        pagesHtml += "<li class=''><a class='page-link bg-secondary text-light page-number'>"+ i +"</a></li>"
-    };
-    pagesHtml += "<li class='page-item'><a class='page-link bg-secondary text-light'>Suivant</a></li>"
-    $("ul.pagination").html(pagesHtml);
+
+    // Pages Links
+    pagesLinksHtml()
     $allLinks     = $('ul.pagination li a') // $allLinks = $pagesLinks + $nextLink + $previousLink
     $pagesLinks   = $('ul.pagination li a.page-number')
     $nextLink     = $($allLinks[$allLinks.length-1])
     $previousLink = $($allLinks[0])
-    // Start by making the page 1 link look active
-    $($pagesLinks[0]).addClass("bg-dark");
-
-    // The previous button is disabled in the beginning
-    $previousLink.parent().css("cursor", "default")
+    $($pagesLinks[0]).addClass("bg-dark"); // Page 1 link looks active
+    $previousLink.parent().css("cursor", "default") // The previous button is disabled in the beginning
     $previousLink.parent().addClass("disabled")
-    eventlistenerlinks()
+    eventsHandlingLinks()
 })
-
 
 //Autocompletion for emails
 $.get("/emails", function(data){
@@ -165,3 +156,5 @@ $.get("/emails", function(data){
 // Possibility to modifiy rowsPerPage
 // Search
 // max length input (30 chars expediteur)
+// Set the default value of the select input to 10 ? Or store it somewhere to retrieve it after reloading the page.
+// Problem when setting rowsperpage to 10 then reloading page : it stays at 10 without and displays only 5 + dosen't do nothing if we click on 10 again
